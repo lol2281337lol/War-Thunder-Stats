fd74756984240c1fe6b4cccf5e573aecfe7603f6
lol2281337lol <37482154+lol2281337lol@users.noreply.github.com>
2023-05-17 23:24:10 +0200

    content.js

--- content.js
+++ content.js
@@ -7,18 +7,18 @@ async function fetchPage(url, account) {
       'Content-Type': 'application/json'
     },
   });
-
   if (!response.ok) {
     throw new Error('Failed to fetch');
   }
-
   return response.json();
 }
 
-
-function parseVehicles(htmlArray) {
-    const airVehicles = [];
-    const landVehicles = [];
+function parseVehicles(htmlArray, vehiclesData) {
+  let gjnSum = 0;
+  let zoSum = 0;
+  const airVehicles = [];
+  const landVehicles = [];
+  const premiumVehicles = []; // новый массив
 
     htmlArray.forEach((html, index) => {
         //console.log(`HTML content at index ${index}:`, html);
@@ -39,6 +39,30 @@ function parseVehicles(htmlArray) {
                 winRate: vehicleWinRate,
                 battles: vehicleBattles,
             };
+		  const fileVehicle = vehiclesData.find(v => v.name === vehicleName);
+  if (fileVehicle) {
+    vehicleData.fileData = fileVehicle;
+    switch (fileVehicle.currency) {
+      case 'З.О':
+        vehicleData.color = '#008000';
+        premiumVehicles.push(vehicleData); // добавляем в премиумные
+        zoSum += Number(fileVehicle.price); // Переместил строку сюда
+        break;
+      case 'GJN':
+        vehicleData.color = 'rgb(200, 22, 193)';
+        premiumVehicles.push(vehicleData); // добавляем в премиумные
+        gjnSum += Number(fileVehicle.price); // Переместил строку сюда
+        break;
+      case 'Пакетная/Акционная':
+        vehicleData.color = '#ee2727';
+        premiumVehicles.push(vehicleData); // добавляем в премиумные
+        break;
+      case 'Наградная':
+        vehicleData.color = '#42aaff';
+        premiumVehicles.push(vehicleData); // добавляем в премиумные
+        break;
+    }
+  }
 
             if (row.dataset.role.includes('bombers all') || row.dataset.role.includes('fighters all')) {
                 airVehicles.push(vehicleData);
@@ -48,7 +72,7 @@ function parseVehicles(htmlArray) {
         });
     });
 
-    return { airVehicles, landVehicles };
+      return { airVehicles, landVehicles, premiumVehicles, gjnSum, zoSum };
 }
 
 
@@ -71,32 +95,115 @@ async function fetchAndLogVehicles(account) {
     }
     try {
         const data = await fetchPage(null, account);
-        const htmlArray = data.data; 
+        const htmlArray = data.data;
+        const vehiclesData = data.vehiclesData;
+
         console.log('Fetched HTML array:', htmlArray);
-        const result = await parseVehicles(htmlArray);
 
-        const airVehiclesTable = createTable(result.airVehicles, 'Воздушная техника');
+        const result = await parseVehicles(htmlArray, vehiclesData);
+
+        const airVehiclesTable = createTable(result.airVehicles, 'Air Vehicles');
 		airVehiclesTable.style.fontSize = '15px';
-		const landVehiclesTable = createTable(result.landVehicles, 'Наземная техника');
+        const landVehiclesTable = createTable(result.landVehicles, 'Land Vehicles');
 		landVehiclesTable.style.fontSize = '15px';
-const mainInfoContainer = document.querySelector('.marketItemView--mainInfoContainer');
-if (mainInfoContainer) {
-    const gridContainer = document.createElement('div');
-    gridContainer.style.display = 'grid';
-    gridContainer.style.gridTemplateColumns = '1fr 1fr';
+        const premiumVehiclesTable = createTable(result.premiumVehicles, 'Premium Vehicles');
 
-    // First append the land vehicles, then the air vehicles
-    gridContainer.appendChild(landVehiclesTable);
-    gridContainer.appendChild(airVehiclesTable);
+        const airVehiclesBattles = result.airVehicles.reduce((sum, vehicle) => sum + parseInt(vehicle.battles), 0);
+        const landVehiclesBattles = result.landVehicles.reduce((sum, vehicle) => sum + parseInt(vehicle.battles), 0);
+        const totalBattles = airVehiclesBattles + landVehiclesBattles;
 
-    mainInfoContainer.appendChild(gridContainer);
+        const airVehiclesPercentage = ((airVehiclesBattles / totalBattles) * 100).toFixed(2);
+        const landVehiclesPercentage = ((landVehiclesBattles / totalBattles) * 100).toFixed(2);
 
+        const tabStructure = createTabStructure(
+            ['Воздушная техника', 'Наземная техника', 'Премиумная техника'],
+            [airVehiclesTable, landVehiclesTable, premiumVehiclesTable],
+            [result.airVehicles.length, result.landVehicles.length, result.premiumVehicles.length], // количество техники
+            [airVehiclesBattles, landVehiclesBattles, 0], // общее количество боёв
+            [airVehiclesPercentage, landVehiclesPercentage, 0] // процентное соотношение боёв
+        );
+
+        const mainInfoContainer = document.querySelector('.marketItemView--mainInfoContainer');
+        if (mainInfoContainer) {
+            const gjnCounter = createCounterElement(result.gjnSum, 'GJN');
+            const zoCounter = createCounterElement(result.zoSum, 'З.О');
+
+            mainInfoContainer.appendChild(gjnCounter);
+            mainInfoContainer.appendChild(zoCounter);
+            mainInfoContainer.appendChild(tabStructure);
+        }
+
+    } catch (error) {
+        console.error('Error fetching vehicles for account', account, error);
     }
+}
 
-} catch (error) {
-    console.error('Error fetching vehicles for account', account, error);
+
+function createCounterElement(value, label) {
+    const element = document.createElement('div');
+    element.style.marginTop = '10px';
+    element.style.marginBottom = '10px';
+
+    const valueElement = document.createElement('span');
+    valueElement.textContent = value;
+    valueElement.style.fontSize = '19px';
+
+    const labelElement = document.createElement('span');
+    labelElement.textContent = ` ${label}`;
+    labelElement.style.fontSize = '14px';
+
+    element.appendChild(valueElement);
+    element.appendChild(labelElement);
+
+    return element;
 }
+
+
+
+function createTabStructure(tabLabels, tabContents, tabCounts, battlesCounts, battlesPercentages) {
+    const tabsContainer = document.createElement('div');
+    tabsContainer.classList.add('tabsContainer');
+
+    const tabsList = document.createElement('ul');
+    tabsList.classList.add('tabsList');
+
+    const tabContentContainer = document.createElement('ul');
+    tabContentContainer.classList.add('tabContentContainer');
+
+    tabLabels.forEach((label, index) => {
+        const tabListItem = document.createElement('li');
+        const tabContentItem = document.createElement('li');  // create the content item
+
+        // add the corresponding content to the content item
+        tabContentItem.appendChild(tabContents[index]);
+
+        if (index < 2) { // если это воздушная или наземная техника
+            tabListItem.textContent = `${label} (${tabCounts[index]}) (${battlesCounts[index]} битв) (${battlesPercentages[index]}%)`;
+        } else { // если это премиумная техника
+            tabListItem.textContent = `${label} (${tabCounts[index]})`;
+        }
+
+        tabListItem.addEventListener('click', () => {
+            document.querySelectorAll('.tabsList li').forEach(li => li.classList.remove('active'));
+            tabListItem.classList.add('active');
+
+            document.querySelectorAll('.tabContentContainer li').forEach(li => li.style.display = 'none');
+            tabContentItem.style.display = 'block';
+        });
+
+        tabsList.appendChild(tabListItem);
+        tabContentContainer.appendChild(tabContentItem); // add the content item to the container
+    });
+
+    tabsContainer.appendChild(tabsList);
+    tabsContainer.appendChild(tabContentContainer);
+
+    return tabsContainer;
 }
+
+
+
+
 
 
 window.addEventListener('load', () => {
@@ -122,25 +229,53 @@ function createTable(dataArray, tableName) {
     const winRateHeader = document.createElement('th');
     winRateHeader.textContent = 'Win Rate     ';
     const battlesHeader = document.createElement('th');
-    battlesHeader.textContent = 'Battles';
+    battlesHeader.textContent = 'Battles           ';
+	const currencyHeader = document.createElement('th'); // новый столбец
+	currencyHeader.textContent = 'Валюта           ';
+	const priceHeader = document.createElement('th'); // новый столбец
+	priceHeader.textContent = 'Цена           ';
+	
     headerRow.appendChild(nameHeader);
     headerRow.appendChild(winRateHeader);
     headerRow.appendChild(battlesHeader);
+	headerRow.appendChild(currencyHeader); // добавляем новый столбец
+	headerRow.appendChild(priceHeader); // добавляем новый столбец
     table.appendChild(headerRow);
 
-    dataArray.forEach((dataItem) => {
-        const row = document.createElement('tr');
-        const nameCell = document.createElement('td');
-        nameCell.textContent = dataItem.name;
-        const winRateCell = document.createElement('td');
-        winRateCell.textContent = dataItem.winRate;
-        const battlesCell = document.createElement('td');
-        battlesCell.textContent = dataItem.battles;
-        row.appendChild(nameCell);
-        row.appendChild(winRateCell);
-        row.appendChild(battlesCell);
-        table.appendChild(row);
-    });
+dataArray.forEach((dataItem) => {
+    const row = document.createElement('tr');
+    const nameCell = document.createElement('td');
+    nameCell.textContent = dataItem.name;
+    const winRateCell = document.createElement('td');
+    winRateCell.textContent = dataItem.winRate;
+    const battlesCell = document.createElement('td');
+    battlesCell.textContent = dataItem.battles;
+
+    // Добавляем новые ячейки
+    const currencyCell = document.createElement('td'); 
+    const priceCell = document.createElement('td'); 
+
+    // Если у данных техники есть информация о файле
+    if (dataItem.fileData) {
+        currencyCell.textContent = dataItem.fileData.currency;
+        priceCell.textContent = dataItem.fileData.price;
+    }
+
+    row.appendChild(nameCell);
+    row.appendChild(winRateCell);
+    row.appendChild(battlesCell);
+    row.appendChild(currencyCell); // добавляем новую ячейку в строку
+    row.appendChild(priceCell); // добавляем новую ячейку в строку
+
+    // Применяем цвет, если он существует
+    if (dataItem.color) {
+        //row.style.backgroundColor = dataItem.color;
+		row.style.color = dataItem.color;
+        row.title = `${dataItem.fileData.name} ; ${dataItem.fileData.price} ; ${dataItem.fileData.currency}`;
+    }
+
+    table.appendChild(row);
+});
 
     const tableContainer = document.createElement('div');
     const tableNameElement = document.createElement('h2');
